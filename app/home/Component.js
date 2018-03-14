5842b9ebe92ad8f96f74f06d6cd5021ce4f237b5
Antony Baasandorj <antony.baasan@gmail.com>
2018-03-14 16:56:25 -0400

    add needed packages

--- app/home/Component.js
+++ app/home/Component.js
@@ -1,10 +1,13 @@
-import React from "react-native";
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-export default class Home extends Component{
-    
-    render(){
-        return (
-            <div>hello world</div>
-        );
-    }
+export default class Home extends Component {
+  render() {
+    return (
+      <View>
+        <button>Execute Shell</button>
+        <button>Speech</button>
+      </View>
+    );
+  }
 }
